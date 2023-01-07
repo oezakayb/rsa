dc9a4f369958c99bde93f4291372d4cf4743c580
Barış Özakay <ge65bej@mytum.de>
2023-01-07 17:37:45 +0100

    correction

---
+++ Implementierung/prime_generator.c
@@ -0,0 +1,30 @@
+#include "prime_generator.h"
+
+void generate_primes(unsigned long *p, unsigned long *q){
+    uint64_t prand;
+    uint64_t qrand;
+    do{
+        uint64_t r1 = (uint64_t) arc4random();
+        uint64_t r2 = (uint64_t) arc4random();
+        prand = r1 << 32 | r2;
+    } while(is_prime(prand) == 0);
+
+    uint64_t max = ((2 >> 64) - 1) / prand;
+
+    do{
+        uint64_t r1 = (uint64_t) arc4random();
+        uint64_t r2 = (uint64_t) arc4random();
+        qrand = r1 << 32 | r2;
+    } while(qrand <= max && is_prime(qrand) == 0);
+
+    *p = prand;
+    *q = qrand;
+}
+
+int is_prime(unsigned long n){
+    if (n <= 1) return 0;
+    for (unsigned long i = 2; i <= sqrt(n); i++) {
+        if (n % i == 0) return 0;
+    }
+    return 1;
+}
--- Implementierung/rsa_params.h
+++ Implementierung/rsa_params.h
@@ -1,1 +1,3 @@
+#include <stdlib.h>
+
 void get_rsa_params(unsigned long* e, unsigned long* d, unsigned long* N);
