a0b2a653becd8bb4af983d3cbfa9169701b840c7
LInq <ge65rux@tum.de>
2023-01-10 10:29:23 +0100

    Lamda function

--- Implementierung/Carmichaels_ Function.c
+++ Implementierung/Carmichaels_ Function.c
@@ -3,58 +3,65 @@
 #include <math.h>
 
 long gcd(unsigned long a, unsigned long b) {
-    for (i = 1; i <= n1 && i <= n2; ++i)
+    for (unsigned int i = 1; i <= a && i <= b; ++i)
     {
         // Checks if i is factor of both integers
-        if (n1 % i == 0 && n2 % i == 0)
-        return i;
-    }
-    return 0;
-}
-long is_gcd(unsigned long a, unsigned long b) {
-    for (i = 1; i <= n1 && i <= n2; ++i)
-    {
-        // Checks if i is factor of both integers
-        if (n1 % i == 0 && n2 % i == 0)
+        if (a % i == 0 && b % i == 0)
             return 1;
     }
     return 0;
 }
- int Carmichael_Function(unsigned long n) {
-     int k;
-     int x;
-     int coprimes[];
-     int counter = 0;
+int Carmichael_Function(unsigned long n) {
+    int k;
+    int* coprimes;
+    //int coprimes[n];
+    int counter = 0;
+    int* ptr;
+    ptr = (int*)malloc(sizeof(int) * n);
+    if (ptr == NULL) {
+        return 1;
+    }
 
-     //find all coprimes of n
-     for (x = 1; x < n; x++) {
-         if (is_gcd(x, n) == 1)
-         {
-             coprimes[counter] = x;
-             counter++;
-         } 
-     }
-     k = 1;
-     int length = sizeof(counter) / 4;
-    
-     bool done = false;
-     while (true) {
-         for (int u = 0; u < length; u++) {
-             if (pow(coprimes[u], k) % n == 1) {
-                 done = true;
-             }
-             else {
-                 done = false;
-                 k += 1;
-                 break;
-             }
-         }
-         if (done) break;
-     }
-    
-        
-     return k;
+
+    //////////////////////////////////////////////////////////////////////////////
+    //find all coprimes of n
+    for (int x = 1; x < n; x++) {
+        if (gcd(x, n) == 1)
+        {
+            //coprimes[counter] = x;
+            //counter++;
+            //coprimes = &x;
+            //*(coprimes+1);
+            //counter++
+
+            ptr[counter] = x;
+            counter++;
+        }
+    }
+
+    k = 1;
+    int length = sizeof(counter) / sizeof(int);
+
+    /////////////////////////////////////////////////////////////////////////////
+
+
+    bool done = false;
+    while (true) {
+        for (int u = 0; u < length; u++) {
+            int temp = pow(ptr[u], k);
+            int check = temp % n;
+            if (check == 1) {
+                done = true;
+            }
+            else {
+                done = false;
+                k += 1;
+                break;
+            }
+        }
+        if (done) break;
+    }
+    free(ptr);
+    return k;
 
 }
-
-
--- Implementierung/Carmichaels_ Function.h
+++ Implementierung/Carmichaels_ Function.h
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <math.h>
 
-void Carmichael_Function(unsigned long N);
+int Carmichael_Function(unsigned long N);
 long gcd(unsigned long a, unsigned long b);
+int main();
+
