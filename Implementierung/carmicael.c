8c096bc5f29cbaef049c7bc2da1886bb3596b21d
LInq <ge65rux@tum.de>
2023-01-14 17:23:24 +0100

    Lamda function in assembly

--- Implementierung/carmicael.c
+++ Implementierung/carmicael.c
@@ -1,25 +1,31 @@
 #include "carmicael.h"
 
-int carmicael(unsigned long n) {
-    int k;
-    for (int i = 2; i < n; i++) {
-        if (gcd(i, n) == 1) {
-            int j = 1;
-            while ((pow(i, j) % n) != 1) {
-                j++;
-            }
-            if (j < k) {
-                k = j;
-            }
-        }
-    }
-    return k;
-}
 
 long gcd(unsigned long a, unsigned long b) {
-    if(b == 0){
+    if (b == 0) {
         return a;
     }
 
-    return gcd(b, a%b);
+    return gcd(b, a % b);
 }
+
+int carmicael(int n) {
+    if (n == 1|| n == 2) {
+        return 1;
+    }
+        int k = 1;
+        for (int i = 2; i < n; i++) {
+            if (gcd(i, n) == 1) {
+                int x = i;
+                int j = 1;
+                while (x != 1) {
+                    x = (x * i) % n;
+                    j++;
+                }
+                k = (k*j)/gcd(k,j);
+            }
+        }
+        return k;
+}
+
+
